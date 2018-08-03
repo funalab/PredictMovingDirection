db0efa023165e8b2bef476940e53a7a178d1e875
Shori Nishimoto <nishimoto@fun.bio.keio.ac.jp>
2018-08-03 18:15:37 +0900

    Fix lint

--- visualization_gbp_paper.sh
+++ visualization_gbp_paper.sh
@@ -3,5 +3,5 @@
 FOLDS=(0 1 2 3)
 
 for n in ${FOLDS[@]}; do
-    python ./src/run_gbp.py ./dataset/NIH3T3_4foldcv/fold$n/test ./results/fold$n/gbp --model_path ./results/fold$n/model.npz -g $1
+    python ./src/run_gbp.py ./dataset_paper/NIH3T3_4foldcv/fold$n/test ./results_paper/fold$n/gbp --model_path ./results_paper/fold$n/model.npz -g $1
 done
